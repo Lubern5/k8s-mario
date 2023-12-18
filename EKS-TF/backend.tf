ed1b6d77184efd5c8c017890f40e4aa78112ed2e
lubern5 <lubern5@yahoo.com>
2023-12-18 13:06:41 -0500

    Mario

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "lu-cloud777" # Replace with your actual S3 bucket name
+    bucket = "lu.123" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
